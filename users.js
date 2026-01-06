3bf1836838b1db407094034e812a03dd8bdc0b03
nbtf-database <bilginikinci@gmail.com>
2026-01-06 21:44:04 +0300

    e

--- users.js
+++ users.js
@@ -37,4 +37,56 @@ const users = [
       { level: "HT2", icon: "medals/shotgun.png" }
     ]
   }
+  
+     {
+    username: "PlayerFour",
+    avatar: "avatars/example.png",
+    shimmer: "shimmers/other.svg",
+    categories: [
+      { level: "LT3", icon: "medals/melee.png" },
+      { level: "LT2", icon: "medals/rifle.png" },
+      { level: "HT1", icon: "medals/pistol.png" },
+      { level: "LT1", icon: "medals/sniper.png" },
+      { level: "HT2", icon: "medals/shotgun.png" }
+    ]
+  },
+  
+     {
+    username: "PlayerFive",
+    avatar: "avatars/example.png",
+    shimmer: "shimmers/other.svg",
+    categories: [
+      { level: "LT3", icon: "medals/melee.png" },
+      { level: "LT2", icon: "medals/rifle.png" },
+      { level: "HT1", icon: "medals/pistol.png" },
+      { level: "LT1", icon: "medals/sniper.png" },
+      { level: "HT2", icon: "medals/shotgun.png" }
+    ]
+  },
+  
+     {
+    username: "PlayerSix",
+    avatar: "avatars/example.png",
+    shimmer: "shimmers/other.svg",
+    categories: [
+      { level: "LT3", icon: "medals/melee.png" },
+      { level: "LT2", icon: "medals/rifle.png" },
+      { level: "HT1", icon: "medals/pistol.png" },
+      { level: "LT1", icon: "medals/sniper.png" },
+      { level: "HT2", icon: "medals/shotgun.png" }
+    ]
+  },
+  
+     {
+    username: "PlayerSeven",
+    avatar: "avatars/example.png",
+    shimmer: "shimmers/other.svg",
+    categories: [
+      { level: "LT3", icon: "medals/melee.png" },
+      { level: "LT2", icon: "medals/rifle.png" },
+      { level: "HT1", icon: "medals/pistol.png" },
+      { level: "LT1", icon: "medals/sniper.png" },
+      { level: "HT2", icon: "medals/shotgun.png" }
+    ]
+  }
 ];
